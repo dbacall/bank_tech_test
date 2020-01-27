9ec5435041a7a7e3998a7bbe72d11e45006afcc9
dbacall <dbacall@hotmail.co.uk>
2020-01-27 15:59:05 +0000

    test: add feature test for withdrawing money

---
+++ spec/features/withdraw_spec.rb
@@ -0,0 +1,10 @@
+
+describe "deposit into account" do
+  it "shows a debit followed by a credit in the statement" do
+    date = Time.now.strftime("%d/%m/%Y")
+    account = Account.new
+    account.deposit(100)
+    account.withdraw(50)
+    expect(account.print_statement).to eq "date || credit || debit || balance\n#{date} ||  || 50.00 || 50.00\n#{date} || 100.00 ||  || 100.00"
+  end
+end
