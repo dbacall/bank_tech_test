1e7ec6541c7e574718688babaaf69cc90d8a5459
dbacall <dbacall@hotmail.co.uk>
2020-01-27 14:21:33 +0000

    feature: add show method to the statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,10 +1,21 @@
 class Statement
   
-  def create_credit(amount, balance)
-    [ Time.now.strftime("%d/%m/%Y"), '%.2f' % amount, "", '%.2f' % balance]
+  def initialize
+    @statement = []
   end
 
-  def create_debit(amount, balance)
-    [ Time.now.strftime("%d/%m/%Y"), "", '%.2f' % amount, '%.2f' % balance]
+  def create_credit(amount, balance_after)
+    @statement << [ Time.now.strftime("%d/%m/%Y"), '%.2f' % amount, "", '%.2f' % balance_after]
+  end
+
+  def create_debit(amount, balance_after)
+    @statement << [ Time.now.strftime("%d/%m/%Y"), "", '%.2f' % amount, '%.2f' % balance_after]
+  end
+
+  def show
+    statement = @statement.reverse.map do |line|
+      line.join(" || ")
+    end
+    "date || credit || debit || balance\n" + statement.join("\n")
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -6,14 +6,22 @@ describe Statement do
 
   describe '#create_credit' do
     it "creates a credit with a date and balance in your statement" do
-      expect(statement.create_credit(100, 100)).to eq [Time.now.strftime("%d/%m/%Y"), "100.00", "", "100.00" ]
+      expect(statement.create_credit(100, 100)).to eq [[Time.now.strftime("%d/%m/%Y"), "100.00", "", "100.00" ]]
     end
   end
 
   describe '#create_debit' do
     it "creates a debit with a date and balance in your statement" do
-      expect(statement.create_debit(100, 100)).to eq [Time.now.strftime("%d/%m/%Y"), "", "100.00", "100.00" ]
+      expect(statement.create_debit(100, 100)).to eq [[Time.now.strftime("%d/%m/%Y"), "", "100.00", "100.00" ]]
     end
   end
 
+  describe '#show' do
+    it 'returns a statement with a debit and credit on' do
+      date = Time.now.strftime("%d/%m/%Y")
+      statement.create_credit(100, 100)
+      statement.create_debit(50, 50)
+      expect(statement.show).to eq("date || credit || debit || balance\n#{date} ||  || 50.00 || 50.00\n#{date} || 100.00 ||  || 100.00")
+    end
+  end
 end
