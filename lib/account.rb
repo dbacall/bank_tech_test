500ee802a1ba2db407519aa80bd16113b3a265bf
dbacall <dbacall@hotmail.co.uk>
2020-01-27 14:47:26 +0000

    feature: add print_statement method to account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,25 @@
+require "statement"
+
 class Account
 
   def initialize
     @balance = 0
+    @statement = Statement.new
   end
 
   def deposit(amount)
     @balance += amount
+    @statement.create_credit(amount, @balance)
+    @balance
   end
 
   def withdraw(amount)
     @balance -= amount
+    @statement.create_debit(amount, @balance)
+    @balance
+  end
+
+  def print_statement
+    @statement.show
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,8 @@ require_relative '../lib/account.rb'
 
 describe Account do
 
-  let(:account) {Account.new}
+  let(:account) { Account.new }
+  let (:date) { Time.now.strftime("%d/%m/%Y") }
 
   describe "#deposit" do
     it "adds 100 to your balance" do 
@@ -14,6 +15,13 @@ describe Account do
     it "takes 50 off your balance" do
       account.deposit(100)
       expect(account.withdraw(50)).to eq 50
+    end
+  end
+
+  describe "#print_statement" do
+    it "returns a statement with 1 credit" do
+      account.deposit(100) 
+      expect(account.print_statement).to eq "date || credit || debit || balance\n#{date} || 100.00 ||  || 100.00"
     end
   end
 
